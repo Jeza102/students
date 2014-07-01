fd68f04fcd30fbe87d7c4eac73878398cd9550eb
Jeza <jezabel102@hotmail.com>
2014-07-01 16:23:49 -0500

    Initial commit

---
+++ personas/Controllers/studentController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using personas.Models;
+
+namespace personas.Controllers
+{
+    public class studentController : Controller
+    {
+        private studentContext db = new studentContext();
+
+        //
+        // GET: /student/
+
+        public ActionResult Index()
+        {
+            return View(db.students.ToList());
+        }
+
+        //
+        // GET: /student/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            student student = db.students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        //
+        // GET: /student/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /student/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.students.Add(student);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(student);
+        }
+
+        //
+        // GET: /student/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            student student = db.students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        //
+        // POST: /student/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(student student)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(student).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(student);
+        }
+
+        //
+        // GET: /student/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            student student = db.students.Find(id);
+            if (student == null)
+            {
+                return HttpNotFound();
+            }
+            return View(student);
+        }
+
+        //
+        // POST: /student/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            student student = db.students.Find(id);
+            db.students.Remove(student);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ personas/Models/student.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace personas.Models
+{
+    public class student
+    {
+        public int studentID { get; set; }
+        public string name { get; set; }
+    }
+}
